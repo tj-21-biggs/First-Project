058cb06f7f1ea1873cad7fd093393874fae11e98
tj-21-biggs <tj.21.biggs@outlook.com>
2017-07-18 16:37:45 -0400

    beginning of the addition of javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+//*-------------------------------------------------------------
+//*     Document Ready
+//--------------------------------------------------------------
+
+
+$Document.Ready(function() {
+  $(' .js-like').on('click', function(event) {
+    event.preventDefault();
+
+    $(this)
+  });
+})
